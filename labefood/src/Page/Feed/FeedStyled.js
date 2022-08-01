db4151493c296adbffd847b964448d5fc967a8b7
Petrick Grillo <petrick.grillo@gmail.com>
2022-08-01 14:16:12 -0300

    Lista restaurante

--- labefood/src/Page/Feed/FeedStyled.js
+++ labefood/src/Page/Feed/FeedStyled.js
@@ -1,1 +1,67 @@
 import styled from "styled-components";
+
+export const MainContainer = styled.div`
+height:100%;
+width:100%;
+`
+export const MainContainerMap = styled.div`
+padding: 8px 16px 0;
+`
+export const ContainerNameRest = styled.div`
+color: #e86e5a;
+display:flex;
+font-family: Roboto;
+font-size: 16px;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+line-height: normal;
+padding-bottom:10px;
+padding-left:10px;
+`
+export const ContainerFrete = styled.div`
+  color: #b8b8b8;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  padding-bottom:10px;
+  padding-right:10px;
+`
+export const ContainerMin = styled.div`
+  color: #b8b8b8;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  padding-left:10px;
+`
+export const ContainerRenderMain = styled.div`
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+`
+export const ContainerInfoTime = styled.div`
+display:flex;
+justify-content:space-between;
+`
+export const ContainerImage = styled.div`
+padding-bottom:2%;
+`
+export const CaptureOrder = styled.button`
+height:20%;
+border:#b8b8b8 solid 1px;
+background:none;
+margin-bottom:5%;
+border-radius:8px;
+`
+export const Image = styled.img`
+width:328px;
+height:170px;
+border-radius: 7px 7px 0px 0px;
+`
