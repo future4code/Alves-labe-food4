608552431e73cd17511b5a02fcb94bbeaae17db9
Petrick Grillo <petrick.grillo@gmail.com>
2022-08-04 10:52:33 -0300

    Signup Arrumado

--- labefood/src/Hooks/useRequestData.js
+++ labefood/src/Hooks/useRequestData.js
@@ -7,7 +7,7 @@ const useRequestData = (initialData, url, refresh) => {
     const getPosts = () => {
         axios.get(url, {
             headers: {
-                auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InJFVFhOODlKY2JpcmRKVjNPaXQwIiwibmFtZSI6IlBldHJpY2siLCJlbWFpbCI6IlBldHJpY2s0QGZ1dHVyZTQuY29tIiwiY3BmIjoiMTIxLjE1MS4xNTEtMTIiLCJoYXNBZGRyZXNzIjp0cnVlLCJhZGRyZXNzIjoiUi4gQWZvbnNvIEJyYXosIDE3NywgNzEgLSBWaWxhIE4uIENvbmNlacOnw6NvIiwiaWF0IjoxNjU5NjE2NDMwfQ.WBanQTK9PIiUQBB9c7_F4MGrKK60ywcecwsFuGhw3P8'
+                auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Imh4TXVraFpIRm1WSG9hWk1XNVgwIiwibmFtZSI6IlBldHJpY2siLCJlbWFpbCI6IlBldHJpY2s1NEBmdXR1cmU1LmNvbSIsImNwZiI6IjE1Ni40NTEuMTUxLTEyIiwiaGFzQWRkcmVzcyI6dHJ1ZSwiYWRkcmVzcyI6IlIuIEFmb25zbyBCcmF6LCAxNzcsIDcxIC0gVmlsYSBOLiBDb25jZWnDp8OjbyIsImlhdCI6MTY1OTYyMDgxOH0.BdxJfcY7L5mt-jJN7I9xYTbwkHD2FbJZkA6RjY8x1n4'
             }
         }).then((resposta) => {
             setData(resposta.data)
--- labefood/src/Page/Sign-up/Singup.js
+++ labefood/src/Page/Sign-up/Singup.js
@@ -17,9 +17,9 @@ import FormControl from '@material-ui/core/FormControl';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
- import Logo from "../../Img/Logologin.png";
+import Logo from "../../Img/Logologin.png";
 import {
-  ConteinerInput, ConteinerInputSenha,LogoSingUpPage,ButtonLogin,TextConteinerSingUpPage
+  ConteinerInput, ConteinerInputSenha, LogoSingUpPage, ButtonLogin, TextConteinerSingUpPage
 
 } from './SingupStyled'
 
@@ -28,8 +28,6 @@ function Singup() {
   const { } = useContext(GlobalContext)
 
   const { form, pegaDados, limpaCampos } = useForm({ name: "", email: "", cpf: "", password: "", confirmPassword: "" })
-
-
   const body = {
     "name": form.name,
     "email": form.email,
@@ -37,19 +35,19 @@ function Singup() {
     "password": form.password,
     "confirmPassword": form.confirmPassword
   }
-
-  axios.post(`${BASE_URL}rappi4B/singup`, body)
-    .then((res) => {
-      console.log(res.data.token)
-      localStorage.setItem('token', res.data.token)
-      goToFeed(navigate)
-    }).catch((err) => {
-      console.log(err)
-    })
-
-
+  const cadastrarUser = () => {
+    axios.post(`${BASE_URL}rappi4B/signup`, body)
+      .then((res) => {
+        console.log(res.data.token)
+        localStorage.setItem('token', res.data.token)
+        goToFeed(navigate)
+      }).catch((err) => {
+        console.log(err)
+      })
+  }
   const onSubmitForm = (e) => {
     e.preventDefault()
+    cadastrarUser()
     limpaCampos()
   }
   const [values, setValues] = useState({
@@ -77,16 +75,14 @@ function Singup() {
   };
 
   return (
-        
     <div>
-      
-      <ButtonLogin onClick={()=> goToLogin(navigate)} ><ArrowBackIosIcon/></ButtonLogin>
-     <LogoSingUpPage src={Logo}  alt={"Logo Ifuture"} />
-     <TextConteinerSingUpPage>Cadastrar</TextConteinerSingUpPage>
-    <form onSubmit={onSubmitForm} > 
+      <ButtonLogin onClick={() => goToLogin(navigate)} ><ArrowBackIosIcon /></ButtonLogin>
+      <LogoSingUpPage src={Logo} alt={"Logo Ifuture"} />
+      <TextConteinerSingUpPage>Cadastrar</TextConteinerSingUpPage>
+      <form onSubmit={onSubmitForm} >
 
         <ConteinerInput>
-            <TextField
+          <TextField
             name={"name"}
             value={form.name}
             onChange={pegaDados}
@@ -96,11 +92,11 @@ function Singup() {
             margin={"normal"}
             required
             placeholder={"Nome e sobrenome"}
-            />
+          />
         </ConteinerInput>
-
+        
         <ConteinerInput>
-            <TextField
+          <TextField
             name={"email"}
             placeholder={"email@email.com"}
             value={form.email}
@@ -110,103 +106,102 @@ function Singup() {
             fullWidth
             required
             type={"email"}
-            
-            />
+          />
         </ConteinerInput>
 
         <ConteinerInput>
 
-            <TextField
-                name={"cpf"}
-                placeholder={"000.000.000-00"}
-                value={form.cpf}
-                onChange={pegaDados}
-                label={"CPF"}
-                variant={"outlined"}
-                fullWidth
-                type={"cpf"}
-                required
-            />
+          <TextField
+            name={"cpf"}
+            placeholder={"000.000.000-00"}
+            value={form.cpf}
+            onChange={pegaDados}
+            label={"CPF"}
+            variant={"outlined"}
+            fullWidth
+            type={"cpf"}
+            required
+          />
 
         </ConteinerInput>
 
         <ConteinerInputSenha>
 
-        <FormControl variant="outlined">
-            
+          <FormControl variant="outlined">
+
             <InputLabel required htmlFor="outlined-adornment-password">Senha</InputLabel>
             <OutlinedInput
-                id="outlined-adornment-password"
-                type={valueSenha.showConfirmePassword ? 'text' : 'password'}
-                value={form.password}
-                name={"password"}
-                onChange={pegaDados}
-                
-                endAdornment={
-            <InputAdornment position="end">
-                <IconButton
-                
+              id="outlined-adornment-password"
+              type={valueSenha.showConfirmePassword ? 'text' : 'password'}
+              value={form.password}
+              name={"password"}
+              onChange={pegaDados}
+
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+
                     aria-label="toggle password visibility"
                     onClick={handleClickShowConfirme}
                     onMouseDown={handlePassword}
                     edge="end"
-                    >
+                  >
                     {valueSenha.showConfirmePassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
+                  </IconButton>
                 </InputAdornment>
-                }
-                labelWidth={70}
+              }
+              labelWidth={70}
             />
-        </FormControl>
+          </FormControl>
 
         </ConteinerInputSenha>
 
         <ConteinerInput>
 
-        <FormControl variant="outlined">
+          <FormControl variant="outlined">
             <InputLabel required htmlFor="outlined-adornment-password">Confirmar</InputLabel>
             <OutlinedInput
-                id="outlined-adornment-password"
-                required
-                type={values.showPassword ? 'text' : 'password'}
-                value={form.confirmPassword}
-                onChange={pegaDados}
-                name={"confirmPassword"}
-                endAdornment={
-            <InputAdornment position="end">
-                <IconButton
+              id="outlined-adornment-password"
+              required
+              type={values.showPassword ? 'text' : 'password'}
+              value={form.confirmPassword}
+              onChange={pegaDados}
+              name={"confirmPassword"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
                     aria-label="toggle password visibility"
                     onClick={handleClickShowPassword}
                     onMouseDown={handleMouseDownPassword}
                     edge="end"
-                    >
+                  >
                     {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
+                  </IconButton>
                 </InputAdornment>
-                }
-                labelWidth={70}
+              }
+              labelWidth={70}
             />
-        </FormControl>
-            
+          </FormControl>
+
 
         </ConteinerInput>
-    
+
         <ConteinerInput>
 
-             <Button
+          <Button
             type={"submit"}
             fullWidth
             variant={"contained"}
             color={"primary"}
 
-            >
-                Criar
-                
-            </Button>
-    
+          >
+            Criar
+
+          </Button>
+
         </ConteinerInput>
-    </form>
-</div>
+      </form>
+    </div>
   )
 }
 
